e492e0872c120fc0d47b6944bb1280a577a628e6
nijianqi <1198952901@qq.com>
2017-12-25 10:51:00 +0800

    保存日志后台修改

--- extend/service/LogService.php
+++ extend/service/LogService.php
@@ -43,7 +43,7 @@ class LogService {
     public static function write($action = '行为', $content = "内容描述") {
         $request = Request::instance();
         $node = strtolower(join('/', [$request->module(), $request->controller(), $request->action()]));
-        $data = ['ip' => $request->ip(), 'node' => $node, 'username' => session('user.username') . '', 'action' => $action, 'content' => $content,'company_id' => session('company_id')];
+        $data = ['ip' => $request->ip(), 'node' => $node, 'username' => session('user.username') . '', 'action' => $action, 'content' => $content,'company_id' => session('user.company_id')];
         return self::db()->insert($data) !== false;
     }
 
