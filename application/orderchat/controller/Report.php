be2991c136ba3ba145c56f1584cf04f2b0b3178e
nijianqi <1198952901@qq.com>
2018-01-31 10:17:49 +0800

    报表拼音写全

--- application/orderchat/controller/Report.php
+++ application/orderchat/controller/Report.php
@@ -104,7 +104,7 @@ class Report extends BasicAdmin
             ->setDescription('Document for Office 2007 XLSX, generated using PHP classes.')
             ->setKeywords('office 2007 openxml php')
             ->setCategory('Result file');
-        $array=['0'=>'A','1'=>'B','2'=>'C','3'=>'D','4'=>'E','5'=>'F','6'=>'G','7'=>'H','8'=>'I','9'=>'J'];
+        $array=['0'=>'A','1'=>'B','2'=>'C','3'=>'D','4'=>'E','5'=>'F','6'=>'G','7'=>'H','8'=>'I','9'=>'J','10'=>'K','11'=>'L','12'=>'M','13'=>'N','14'=>'O','15'=>'P','16'=>'Q','17'=>'R','18'=>'S','19'=>'T','20'=>'U','21'=>'V','22'=>'W','23'=>'X','24'=>'Y','25'=>'Z'];
         foreach ($name as $key=>$val){
             $objPHPExcel->setActiveSheetIndex(0)->setCellValue($array[$key].'1',$val);
         }
