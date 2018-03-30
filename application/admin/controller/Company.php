63e8a286a9719efcf3ea6ac7940d7070d435c001
nijianqi <1198952901@qq.com>
2018-03-30 15:21:19 +0800

    后台增加支付方式

--- application/admin/controller/Company.php
+++ application/admin/controller/Company.php
@@ -26,7 +26,8 @@ use think\Db;
  * @author Anyon <zoujingli@qq.com>
  * @date 2017/02/15 18:12
  */
-class Company extends BasicAdmin {
+class Company extends BasicAdmin
+{
 
     /**
      * 指定当前数据表
@@ -37,7 +38,8 @@ class Company extends BasicAdmin {
     /**
      * 单位列表
      */
-    public function index() {
+    public function index()
+    {
         // 设置页面标题
         $this->title = '单位信息管理';
         // 获取到所有GET参数
@@ -54,12 +56,13 @@ class Company extends BasicAdmin {
         // 实例化并显示
         return parent::_list($db);
     }
-    
+
 
     /**
      * 单位添加
      */
-    public function add() {
+    public function add()
+    {
         $extendData = [];
         if ($this->request->isPost()) {
             $sqlstr = "exec [up_get_max_id] ?,?,?,?";
@@ -67,42 +70,79 @@ class Company extends BasicAdmin {
             $extendData['company_id'] = $data[0][0]['id'];
         }
         LogService::write('系统管理', '执行单位添加操作');
-        return $this->_form($this->table, 'form','company_id','',$extendData);
+        return $this->_form($this->table, 'form', 'company_id', '', $extendData);
     }
 
     /**
      * 单位编辑
      */
-    public function edit() {
+    public function edit()
+    {
         $extendData = [];
         if ($this->request->isPost()) {
             $data = $this->request->get();
             $extendData['company_id'] = $data['company_id'];
         }
         LogService::write('系统管理', '执行单位编辑操作');
-        return $this->_form($this->table, 'form','','',$extendData);
+        return $this->_form($this->table, 'form', '', '', $extendData);
     }
 
     /**
      * 表单数据默认处理
      * @param array $data
      */
-    public function _form_filter(&$data) {
+    public function _form_filter(&$data)
+    {
         if ($this->request->isPost()) {
+
+            if (isset($data['pay_type']) && is_array($data['pay_type'])) {
+                Db::name('company_list_third_interface')->where(['company_id' => $data['company_id'],'third_type'=>'2'])->delete();
+                foreach ($data['pay_type'] as $key => $val) {
+                    Db::name('company_list_third_interface')->insert(['company_id' => $data['company_id'], 'third_interface_id' => $data['pay_type'][$key],'third_type'=>'2']);
+                }
+                unset($data['pay_type']);
+            } else {
+                Db::name('company_list_third_interface')->where(['company_id' => $data['company_id'],'third_type'=>'2'])->delete();
+            }
+
+            if (isset($data['ic_type'])) {
+                Db::name('company_list_third_interface')->where(['company_id' => $data['company_id'],'third_type'=>'1'])->delete();
+                Db::name('company_list_third_interface')->insert(['company_id' => $data['company_id'], 'third_interface_id' => $data['ic_type'],'third_type'=>'1']);
+                unset($data['ic_type']);
+            }elseif ($data['ic_type'] == '0'){
+                Db::name('company_list_third_interface')->where(['company_id' => $data['company_id'],'third_type'=>'1'])->delete();
+            } else {
+                Db::name('company_list_third_interface')->where(['company_id' => $data['company_id'],'third_type'=>'1'])->delete();
+            }
+
             if (Db::name($this->table)->where('company_id', session('user.company_id'))->find()) {
                 unset($data['Company_Name']);
             } elseif (isset($data['Company_Name']) and Db::name($this->table)->where('Company_Name', $data['Company_Name'])->find()) {
                 $this->error('单位名称已经存在，请使用其它名称！');
             }
-        }else{
+
+        } else {
             $this->assign('company_type', Db::name('company_type')->select());
+            $this->assign('ic_type', Db::name('v_third_ic_list')->select());
+            $this->assign('pay_type', Db::name('v_third_pay_list')->select());
+
+            if (!empty($_GET['company_id'])) {
+                $pay_list = Db::name('company_list_third_interface')->where(['company_id' => $_GET['company_id'],'third_type'=>'2'])->select();
+                $pay_ids = array_column($pay_list, 'third_interface_id');
+                $this->assign('pay_ids', $pay_ids);
+
+                $ic_id = Db::name('company_list_third_interface')->where(['company_id' => $_GET['company_id'],'third_type'=>'1'])->find();
+                $this->assign('ic_id', $ic_id['third_interface_id']);
+            }
+
         }
     }
 
     /**
      * 删除单位
      */
-    public function del() {
+    public function del()
+    {
         LogService::write('系统管理', '执行单位删除操作');
         if (DataService::update($this->table)) {
             $this->success("单位删除成功！", '');
@@ -113,7 +153,8 @@ class Company extends BasicAdmin {
     /**
      * 单位禁用
      */
-    public function forbid() {
+    public function forbid()
+    {
         LogService::write('系统管理', '执行单位禁用操作');
         if (DataService::update($this->table)) {
             $this->success("单位禁用成功！", '');
@@ -124,7 +165,8 @@ class Company extends BasicAdmin {
     /**
      * 单位启用
      */
-    public function resume() {
+    public function resume()
+    {
         LogService::write('系统管理', '执行单位启用操作');
         if (DataService::update($this->table)) {
             $this->success("单位启用成功！", '');
