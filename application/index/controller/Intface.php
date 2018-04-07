284722869cac6934a76b000866200627a85483cb
nijianqi <1198952901@qq.com>
2018-04-07 14:39:17 +0800

    支付优化

--- application/index/controller/Intface.php
+++ application/index/controller/Intface.php
@@ -41,6 +41,8 @@ class Intface extends ApiBase
                Db::table("rechargeOrder_list")->where(['MerSeqNo'=>$_POST['MerSeqNo'],'TransAmt'=>$_POST['TransAmt'],'status'=>'0'])->update(['status'=>'1']);
            }
            return json(['RespCode'=>'000000']);
+       }else{
+           return json(['RespCode'=>'000001']);
        }
     }
 
@@ -48,8 +50,22 @@ class Intface extends ApiBase
      *浙农信支付查询
      */
 
-    private function paySearch(){
-
+    private function paySearch($MerSeqNo){
+        $order_info = Db::table("rechargeOrder_list")->where(['MerSeqNo'=>$MerSeqNo])->find();
+        $data = [];
+        $data['TransId'] = 'IQSR';
+        $data['MerchantId'] = $order_info['MerchantId'];
+        $data['SubMerchantId'] = $order_info['MerchantId'];
+        $data['MerSeqNo'] = $order_info['MerSeqNo'];
+        $data['MerTransDate'] = $order_info['MerDateTime'];
+        $html = post_curls('http://158.222.29.118:8080/merchant/*.do?TransName=prePWDReq',$data);
+        preg_match_all('|value="(.*)"|isU',$html,$arr); //匹配到数组$arr中；
+        $data2 = [];
+        $data2['Plain'] = $arr[1][2];
+        $data2['Signature'] = $arr[1][3];
+        $data2['signedData'] = [];
+        $html = post_curls('http://158.222.29.118:8080/paygate/main',$data2);
+        preg_match_all('|value="(.*)"|isU',$html,$arr); //匹配到数组$arr中；
     }
 
 }
