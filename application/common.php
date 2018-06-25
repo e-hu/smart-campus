b24677ba4ab8b0395ed3162aba1d5b5cc7ff2664
nijianqi <1198952901@qq.com>
2018-06-25 08:35:23 +0800

    发送消息增加company_id

--- application/common.php
+++ application/common.php
@@ -387,7 +387,7 @@ function refundMSC($openid)
         'keyword1'=>array('value'=>'管理员审核'),
         'keyword2'=>array('value'=>date('Y-m-d H:m:i')),
         'remark'=>array('value'=>'点击本条信息进行操作。')
-    ),Request::instance()->domain().'/index/index/checkList');
+    ),Request::instance()->domain().'/index/index/checkList.html?company_id='.$this->getClientCompanyId());
 }
 
 /**
@@ -401,7 +401,7 @@ function returnMSC($openid)
         'keyword1'=>array('value'=>'申请结果通知'),
         'keyword2'=>array('value'=>date('Y-m-d H:m:i')),
         'remark'=>array('value'=>'点击本条信息进行操作。')
-    ),Request::instance()->domain().'/index/index/refund');
+    ),Request::instance()->domain().'/index/index/refund.html?company_id='.$this->getClientCompanyId());
 }
 
 /**
@@ -415,7 +415,7 @@ function patchMSC($openid)
         'keyword1'=>array('value'=>'申请结果通知'),
         'keyword2'=>array('value'=>date('Y-m-d H:m:i')),
         'remark'=>array('value'=>'点击本条信息进行操作。')
-    ),Request::instance()->domain().'/index/index/patch');
+    ),Request::instance()->domain().'/index/index/patch.html?company_id='.$this->getClientCompanyId());
 }
 
 /**
@@ -429,7 +429,7 @@ function refundNoMSC($openid)
         'keyword1'=>array('value'=>'申请结果通知'),
         'keyword2'=>array('value'=>date('Y-m-d H:m:i')),
         'remark'=>array('value'=>'点击本条信息进行操作。')
-    ),Request::instance()->domain().'/index/index/checkList');
+    ),Request::instance()->domain().'/index/index/checkList.html?company_id='.$this->getClientCompanyId());
 }
 
 function search_food($name)
