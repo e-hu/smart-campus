a9f59469bf9ae0e7a9c270296f01af41a6c5bb0a
nijianqi <1198952901@qq.com>
2018-06-28 10:42:21 +0800

    优化上次修改，后台设备二维码异常，微信登录失败，只清空密码

--- application/orderchat/controller/Equipment.php
+++ application/orderchat/controller/Equipment.php
@@ -121,7 +121,7 @@ class Equipment extends BasicAdmin
                 ->setErrorCorrection('high')
                 ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
                 ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
-                ->setLabel($machine_id)
+                ->setLabel($machine_sn)
                 ->setLabelFontSize(16)
                 ->setImageType(\Endroid\QrCode\QrCode::IMAGE_TYPE_PNG);
             $qrCode->render();
